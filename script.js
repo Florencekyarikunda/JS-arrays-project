d62c251a2c4c2451f4ac13fbb4081cbe4b0404b2
Florencekyarikunda <79209854+Florencekyarikunda@users.noreply.github.com>
2021-05-28 16:41:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//colour array
+let colors=['blue','yellow','green','red','grey','black','orange','pupple','pink',''];
+//get button
+let button=document.getElementById('button');
+
+//add event listener
+button.addEventListener('click',function(){
+ var randomColor=colors[Math.floor(Math.random() *colors.length)]
+
+ //get container
+ let container=document.getElementById('container');
+ //randomizer
+ container.style.background=randomColor
+})
